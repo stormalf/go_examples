9c76ceadec05c83da102197df17f466128b055fd
stormalf <stormalf.summonerswar@gmail.com>
2021-10-27 10:13:18 +0200

    removing redirection not working for now

--- https/myhttps.go
+++ https/myhttps.go
@@ -9,18 +9,19 @@ import (
 
 var tlsCertPath = os.Getenv("TLS_CERT_PATH")
 var tlsKeyPath = os.Getenv("TLS_KEY_PATH")
+var httpsAddr = ":8443"
 
-func httpsRedirect(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, "https://"+r.Host+r.URL.String(), http.StatusMovedPermanently)
-}
 
 func main() {
 	//handle '/' route
 	http.HandleFunc("/", func( res http.ResponseWriter, req *http.Request) {
+		if req.URL.Path != "/" {
+			http.NotFound(res, req) 
+			return
+		}
 		fmt.Fprint(res, "Hello World!")
 	})
-	go http.ListenAndServe(":81", http.HandlerFunc(httpsRedirect))
 
 	//run server on port 443
-	log.Fatal(http.ListenAndServeTLS(":8443", tlsCertPath, tlsKeyPath, nil))
+	log.Fatal(http.ListenAndServeTLS(httpsAddr, tlsCertPath, tlsKeyPath, nil))
 }
