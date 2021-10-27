497b9a82b737c13186c6aaadb723fbbf57d9c209
stormalf <stormalf.summonerswar@gmail.com>
2021-10-27 18:49:51 +0200

    adding redirection

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ golang examples for learning
 
 ## https
 
-Basic example of https server in golang
+Basic example of https server in golang with redirecting http to https
--- https/myhttps.go
+++ https/myhttps.go
@@ -10,7 +10,19 @@ import (
 var tlsCertPath = os.Getenv("TLS_CERT_PATH")
 var tlsKeyPath = os.Getenv("TLS_KEY_PATH")
 var httpsAddr = ":8443"
+var httpAddr = ":80"
 
+func redirect(w http.ResponseWriter, req *http.Request) {
+    // remove/add not default ports from req.Host
+    target := "https://" + req.Host + req.URL.Path 
+    if len(req.URL.RawQuery) > 0 {
+        target += "?" + req.URL.RawQuery
+    }
+    log.Printf("redirect to: %s", target)
+    http.Redirect(w, req, target,
+            // see comments below and consider the codes 308, 302, or 301
+            http.StatusPermanentRedirect)
+}
 
 func main() {
 	//handle '/' route
@@ -21,7 +33,8 @@ func main() {
 		}
 		fmt.Fprint(res, "Hello World!")
 	})
+	go http.ListenAndServe(httpAddr, http.HandlerFunc(redirect))
 
-	//run server on port 443
+	//run server on port 8443
 	log.Fatal(http.ListenAndServeTLS(httpsAddr, tlsCertPath, tlsKeyPath, nil))
 }
